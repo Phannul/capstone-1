565d3bd87862846a22a3ee484f673cfc59ae8cac
phannu <fanuelbirhane433@gmail.com>
2025-10-10 14:32:15 -0500

    menu prompter while loop constructed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //a scanner prompt to collect users' menu choice
         Scanner userMenu = new Scanner(System.in);
-        System.out.println("What do you want to do today?\n D) Add deposit \n P) Make Payment \n L) Ledger \n X) Exit");
-        String input = userMenu.nextLine();
+
+
+        boolean running = true;
+        while(running){
+            System.out.println("What do you want to do today?\n D) Add deposit \n P) Make Payment \n L) Ledger \n X) Exit");
+            String choice = userMenu.nextLine().trim();
+            if(choice.equalsIgnoreCase("D")){
+                System.out.println("Deposit method");
+            } else if (choice.equalsIgnoreCase("P")) {
+                System.out.println("Payment Method");
+
+            } else if (choice.equalsIgnoreCase("L")) {
+                System.out.println("ledger method");
+            } else if (choice.equalsIgnoreCase("X")) {
+                running = false;
+            }else {
+                System.err.println("invalid or empty selection detected");
+                continue;
+
+            }
+        }
 
 
     }
