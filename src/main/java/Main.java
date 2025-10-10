5900fc574b1c23d4a84a9a726bc2757d36552105
phannu <fanuelbirhane433@gmail.com>
2025-10-10 14:01:38 -0500

    started with the user choice menu prompter

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner userMenu = new Scanner(System.in);
+        System.out.println("What do you want to do today?\n D) Add deposit \n P) Make Payment \n L) Ledger \n X) Exit");
+        String input = userMenu.nextLine();
+
+
+    }
+}
