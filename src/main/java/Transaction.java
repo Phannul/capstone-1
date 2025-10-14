4139216105bad098dc92b2da250241aefea5cd6b
phannu <fanuelbirhane433@gmail.com>
2025-10-14 08:41:48 -0500

    Added a toString method in the transaction class.

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -2,9 +2,42 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Transaction {
+    //private fields for encapsulation
     private LocalDate date;
     private LocalTime currentTime;
     private String description;
     private String vendor;
     private double amount;
+
+    public Transaction(LocalDate date, LocalTime currentTime, String description, String vendor, double amount) {
+        this.date = date;
+        this.currentTime = currentTime;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getCurrentTime() {
+        return currentTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+    public String toString () {
+        return date + "|" + currentTime + "|" + description + "|" + vendor +"|" + amount ;
+    }
 }
+
