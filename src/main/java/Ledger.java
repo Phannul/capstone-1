3a245932ffd49f77d42f67345eb50180569a235e
phannu <fanuelbirhane433@gmail.com>
2025-10-14 09:18:33 -0500

    Started with the Ledger class and created required methods

---
+++ src/main/java/Ledger.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Ledger {
+    private ArrayList<Transaction> transactionLists;
+
+    public Ledger() {
+        this.transactionLists = transactionLists;
+    }
+    public void addTransaction(Transaction t) {
+        transactionLists.add(t);
+        // commands deposit and payment
+    }
+    public void showEverything(){
+        // shows every transaction
+    }
+    public void showDeposits(){
+        // shows deposited lists
+    }
+    public void showPayments(){
+        //shows only payment or credited amounts
+    }
+    public void showReports(){
+        //shows reports
+
+    }
+
+}
